5c71826eeb919810cb59fdf77aa80eb736fd29f2
Dom2105 <Domi.rittig@gmail.com>
2022-01-10 15:38:19 +0100

    Update artikel-Impl.php

--- artikel-Impl.php
+++ artikel-Impl.php
@@ -5,3 +5,5 @@ $name = $_POST["artikelname"];
 $preis = $_POST["artikelpreis"];
 
 $conn->query("INSERT INTO artikel (artikelname, preis) VALUES ('$name', $preis)");
+
+header("Location: http://localhost/php/artikel.php");
